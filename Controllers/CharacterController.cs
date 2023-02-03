7d6f6d5064f2df779dc8f6e8db6778612da4f001
Farukcan Şoray <Farukcan.Soray@avl.com>
2023-02-03 16:36:04 +0300

    delete

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -32,9 +32,18 @@ namespace dotnet_project.Controllers
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
 
-         [HttpPut]
+        [HttpPut]
         public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> UpdateCharacter(UpdateCharacterDto updatedCharacter){
             var response = await _characterService.UpdateCharacter(updatedCharacter);
+            if(response.Data is null){
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> DeleteCharacter(int id){
+            var response = await _characterService.DeleteCharacter(id);
             if(response.Data is null){
                 return NotFound(response);
             }
