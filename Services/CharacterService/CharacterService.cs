30cc4a2907d90418fe49f13d8539ffd44a77021d
Farukcan Şoray <Farukcan.Soray@avl.com>
2023-02-03 14:15:28 +0300

    async

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -9,20 +9,24 @@ namespace dotnet_project.Services.CharacterService
     {
         private static List<Character> characters = new List<Character>{new Character(), new Character{ Id = 1, Name = "Sam"}};
 
-        public List<Character> AddCharacter(Character newCharacter)
+        public async Task<List<Character>> AddCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
             return characters;
         }
 
-        public List<Character> GetAllCharacters()
+        public async Task<List<Character>> GetAllCharacters()
         {
             return characters;
         }
 
-        public Character GetCharacterById(int id)
+        public async Task<Character> GetCharacterById(int id)
         {
-            return characters.FirstOrDefault(c => c.Id == id);
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            if(character is not null){
+                return character;
+            } 
+            throw new Exception("Character not found");
         }
     }
 }
