b61b0b2bb2b9d9f2712e6168393d0440fe1595b3
Farukcan Şoray <Farukcan.Soray@avl.com>
2023-02-03 13:21:34 +0300

    parameter routing

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,16 +10,16 @@ namespace dotnet_project.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character>{new Character(), new Character{Name = "Sam"}};
+        private static List<Character> characters = new List<Character>{new Character(), new Character{ Id = 1, Name = "Sam"}};
 
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get(){
             return Ok(characters);
         }
 
-        [HttpGet("GetSingle")]
-        public ActionResult<Character> GetSingle(){
-            return Ok(characters[0]);
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id){
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }
