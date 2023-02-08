02be5f6dabf9043b1e1a200ef55c82d335ae2db3
Farukcan Şoray <Farukcan.Soray@avl.com>
2023-02-08 16:36:50 +0300

    data context

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 global using dotnet_project.Models;
 global using dotnet_project.Services.CharacterService;
 global using dotnet_project.Dtos.Character;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_project.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
