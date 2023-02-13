c8fed1da7cee424f9c4d9949de218c38945b275a
Farukcan Şoray <Farukcan.Soray@avl.com>
2023-02-13 17:14:01 +0300

    delete in db

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -8,7 +8,6 @@ namespace dotnet_project.Services.CharacterService
 {
     public class CharacterService : ICharacterService
     {
-        private static List<Character> characters = new List<Character>{new Character(), new Character{ Id = 1, Name = "Sam"}};
         private readonly IMapper _mapper;
         private readonly DataContext _context;
 
@@ -34,10 +33,14 @@ namespace dotnet_project.Services.CharacterService
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
 
             try{         
-            var character = characters.First(c => c.Id == id);
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            if(character is null){
+                throw new Exception($"Character with Id '{id}' not found.");
+            }
 
-            characters.Remove(character);
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            _context.Characters.Remove(character);
+            await _context.SaveChangesAsync();
+            serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
             }
             catch(Exception ex){
                 serviceResponse.Success = false;
